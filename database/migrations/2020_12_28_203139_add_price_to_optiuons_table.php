d66a78a857e1a0cdc1e9b800d68426044973f9c1
dddd <moahemmd.hatem100100@gmial.com>
2020-12-28 20:08:08 -0100

    db options

---
+++ database/migrations/2020_12_28_203139_add_price_to_optiuons_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPriceToOptiuonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+ 
+        Schema::table('options', function (Blueprint $table) {
+            $table->string('price')->nullable()->after('product_id');
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('options', function (Blueprint $table) {
+            $table->dropColumn('price');
+        });
+    }
+}
